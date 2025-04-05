11b4fd8c4b2673af7484006704688fb53514f2e8
Annariia <167634401+annikon3@users.noreply.github.com>
2025-04-05 17:14:53 +0300

    cors

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
+
+# Sources: https://docs.python.org/3/library/pickle.html#module-pickle
 
 # Load the model pipeline (pickled model from Google Colab)
 with open("sentiment_pipeline.pkl", "rb") as f:
     model = pickle.load(f)
 
+
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
-def hello_world():
-    return "<h1>Sentiment Analysis v1 PROD<h1>"
+def home():
+    return "<h1>Sentiment Analysis v1.2 PROD<h1>"
+
 
 @app.route('/sentiment', methods=['POST'])
 def predict_sentiment():
-    input_data = request.json
-    text = input_data.get("text", "")
+    data = request.json
+    text = data.get("text", "")
 
     if not text:
         return jsonify({"error": "No text provided"}), 400
@@ -22,8 +29,7 @@ def predict_sentiment():
     prediction = model.predict([text])[0]
 
     return jsonify({"result": prediction})
-    
-    # return {"input_data": input_data, "message": "hello!"}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=False)
